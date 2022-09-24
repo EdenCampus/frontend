2a442cd636023f38014b4be2286f87946d81e06e
dasimems <isaacseun63@gmail.com>
2022-09-24 09:38:16 -0400

    perfected the review left scroll  button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,13 +5,14 @@ function scrollReviewLeft(){
         allContainer = All(".review-contents .review"),
         containerWidth = container.getBoundingClientRect().width,
         scrollLength = ((-1 * (containerWidth + 30)) + (allContainer[0].offsetLeft)),
-        fullScrollLength = (containerWidth + 30);
+        fullScrollLength = (containerWidth + 30) * allContainer.length;
+
+        if(fullScrollLength + scrollLength < 30){
+            scrollLength = allContainer[0].offsetLeft;
+
+        }
 
         allContainer[0].style.marginLeft = scrollLength + "px";
-
-
-
-
 
 
     console.dir(scrollLength)
