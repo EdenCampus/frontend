50dce13483374a0c9946f9d8647448461f901888
dasimems <isaacseun63@gmail.com>
2022-09-25 10:53:49 -0400

    finished the show and hide password button

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,26 +1,74 @@
 "use strict";
 
 function showPassword(e){
-    var passwordInput = e.target.attributes;
+    var passwordInput = e.target,
+        showPasswordButton = All(".show-password");
 
-    if(passwordInput["data-class"]){
-        var inputElement = Ele("input#" + passwordInput["data-class"].value)
+    if(passwordInput.hasAttribute("data-class")){
+
+        var inputElement = Ele("input#" + passwordInput.getAttribute("data-class"))
 
         // console.log(inputElement)
         inputElement.focus();
         if(inputElement.type !== "password"){
+
             inputElement.type = "password";
+            addClass("remove");
+            
         }else{
             
+            addClass("add");
             inputElement.type = "text";
         }
 
+        function addClass(action){
+            switch(action){
+                case "add":
+                    showPasswordButton.forEach((element)=>{
+            
+                        if(element.hasAttribute("data-class")){
+            
+                            if(element.getAttribute("data-class") === passwordInput.getAttribute("data-class")){
+                                element.classList.add("password-shown")
+            
+                            }
+                        }
+                    })
+                    return;
+
+
+                case "remove":
+
+                    showPasswordButton.forEach((element)=>{
+            
+                        if(element.hasAttribute("data-class")){
+            
+                            if(element.getAttribute("data-class") === passwordInput.getAttribute("data-class")){
+                                element.classList.remove("password-shown")
+            
+                            }
+                        }
+                    })
+                    return;
+
+                default:
+                    return;
+
+
+            }
+        }
+
+        
+
+
+
     }
 
-    console.log(passwordInput["data-class"].value)
+    // console.log(passwordInput["data-class"].value)
     
     
 
+    
 }
 
 window.addEventListener("load", ()=>{
