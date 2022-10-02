095e92bb62410802e2b093dc3d9b7ef8a1256ca1
dasimems <isaacseun63@gmail.com>
2022-10-01 21:38:48 -0400

    done with the header section in the dashboard page

---
+++ user/includes/header.php
@@ -0,0 +1,33 @@
+<header>
+
+    <div class="welcome-message">
+
+        <p class="message">Welcome,</p>
+
+        <div class="profile-details">
+            
+            <div class="image">
+
+                <img src="./assets/images/profile_images/profile_image.jpg" alt="">
+
+            </div>
+
+            <p class="name">Duyil</p>
+    
+        </div>
+    
+    </div>
+
+    <div class="header-icon">
+
+        <ul class="link-btn">
+
+            <li><button class="header-link-btn" data-action="dark-mode"><i class="fa-regular fa-moon"></i></button></li>
+            <li><button class="header-link-btn" data-action="notification"><i class="fa-regular fa-bell"></i></button></li>
+            <li><button class="header-link-btn" data-action="settings"><i class="fa-solid fa-gear"></i></button></li>
+
+        </ul>
+
+    </div>
+
+</header>
